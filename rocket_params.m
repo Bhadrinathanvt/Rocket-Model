d93f9ede446c9e3b92e4497ad9c55916660452e0
Bhadrinathanvt <133013526+Bhadrinathanvt@users.noreply.github.com>
2025-08-03 02:40:30 +0530

    Added rocket parameters file

---
+++ rocket_params.m
@@ -0,0 +1,10 @@
+% rocket_params.mat
+dry_mass = 17.190; % Kg
+full_mass = 23.872; % Kg                          
+drag_coefficient = 0.5; % unit-less
+diameter = 0.1524; % m (6in)
+Length = 2.6416; % m (104in)
+launch_wobble = [0, 0, 0.1]; % rad/s
+launch_angle = [0, 0, 0]; % rad
+
+    
